adb0db85a95fb061e7f3b968c8c40029474b7274
Kai120789 <zoomer120789@gmail.com>
2024-09-05 15:15:41 +0300

    new

--- base/base/settings.py
+++ base/base/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,7 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Конфигурация сервера электронной почты
 EMAIL_HOST = 'smtp.mail.ru'
 EMAIL_HOST_USER = 'kinglaval96@mail.ru'
-EMAIL_HOST_PASSWORD = 'unRL3nyhwY6wjBDnj38v'
+EMAIL_HOST_PASSWORD = os.getenv('email_host_password')
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
